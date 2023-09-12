1f985fdb4f29c241bcea1aae28bd0ccfba316619
Valentine <iosadcbmzx@protonmail.com>
2023-09-12 16:57:13 +0700

    Middleware

---
+++ src/1. API/KeycloakUserService.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace KeycloakUserService.API.Middlewares;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next.Invoke(context);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}        
